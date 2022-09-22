8c0c0e91d343c3c9dc2854d6a69f00247b7be5b9
jadwin1997 <jadwinmanuel@gmail.com>
2022-09-22 16:52:49 -0500

    Add files via upload

---
+++ EXAMPLES/motor_blank_code/motor_blank_code.ino
@@ -0,0 +1,20 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
+Adafruit_DCMotor *motor1 = AFMS.getMotor(1);
+Adafruit_DCMotor *motor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *motor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *motor4 = AFMS.getMotor(4);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  AFMS.begin();
+  motor1->setSpeed(255);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  motor1->run(FORWARD);
+}
+
