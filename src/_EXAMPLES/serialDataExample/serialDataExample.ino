039d0ac720753869bf2e329bec041b4f4edae0c5
jadwin1997 <jadwinmanuel@gmail.com>
2022-10-20 15:52:38 -0500

    Create serialDataExample.ino

---
+++ src/_EXAMPLES/serialDataExample/serialDataExample.ino
@@ -0,0 +1,34 @@
+void setup() {
+  // put your setup code here, to run once:
+
+//Start serial monitor
+Serial.begin(9600);
+
+//Set up LED pin (as you can see, we are using the arduinos built-in LED)
+pinMode(LED_BUILTIN,OUTPUT);
+}
+
+//Create time_delay variable
+int time_delay = 1000;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//Write light high  
+digitalWrite(LED_BUILTIN,HIGH);
+//Delay
+delay(time_delay);
+//Write light low
+digitalWrite(LED_BUILTIN,LOW);
+//Delay
+delay(time_delay);
+
+//If serial data available, update the time_delay
+if(Serial.available()>0){
+if(Serial.read()=='x'){
+  //wait for x, then save number after it.
+  time_delay = Serial.parseInt();
+}
+}
+}
+
+
